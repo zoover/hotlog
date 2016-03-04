4ef38d6a3ca427013d02b73de829166895a6850c
tikider <mbmbrk@gmail.com>
2016-03-04 09:26:31 +0100

    local log file only

--- index.js
+++ index.js
@@ -1,45 +1,36 @@
 var bunyan = require('bunyan');
 var fs = require('fs');
-
 var logger;
 var runningScript = require.main.filename.split('/').pop();
 var rootDirPath = require.main.filename.split('/').slice(0, -1).join('/');
 var logDirPath = rootDirPath + "/log";
-var localLogFilePath = __dirname + "/" + runningScript + '.log';
-var logFilePath = process.env.APP_LOG_FILE_PATH || localLogFilePath;
-
-
+var logFilePath = logDirPath + "/" + runningScript + ".log";
 var streams = [];
-var logDirStats = fs.statSync(logDirPath);
-if (logDirStats.isDirectory()) {
-  streams.push({
 
-  });
+try {
+  var logDirStats = fs.statSync(logDirPath);
+  if (logDirStats.isDirectory()) {
+    // create file if not exists
+    var fd = fs.openSync(logFilePath, 'a');
+    fs.closeSync(fd);
+
+    streams.push({
+      level: 'info',
+      path: logFilePath
+    });
+  }
+} catch (e) {
+  console.log(e);
 }
-streams.push();
 
-
-// create file if not exists
-var fd = fs.openSync(logFilePath, 'a');
-fs.closeSync(fd);
+streams.push({
+  level: 'info',
+  stream: process.stdout
+});
 
 logger = bunyan.createLogger({
   name: runningScript,
-  streams: [
-    {
-      level: 'info',
-      stream: process.stdout
-    },
-    {
-      level: 'info',
-      path: logFilePath
-    }
-  ]
+  streams: streams
 });
-
-logger.info('A normal operation going on');
-logger.warn('this is a warning');
-logger.debug('looking for source of a bug');
-logger.error('something horrible just happened');
 
 module.exports = logger;
