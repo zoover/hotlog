640504fe94ff0f0a39082656c9f44fd49c43acf4
Gerben Meyer <g.meyer@zoover.nl>
2016-11-09 15:37:44 +0100

    only log to stdout and stderr

--- build/logger.js
+++ build/logger.js
@@ -8,10 +8,6 @@ var _bunyan = require('bunyan');
 
 var _bunyan2 = _interopRequireDefault(_bunyan);
 
-var _fs = require('fs');
-
-var _fs2 = _interopRequireDefault(_fs);
-
 var _stdIOStream = require('./stdIOStream');
 
 var _stdIOStream2 = _interopRequireDefault(_stdIOStream);
@@ -31,24 +27,7 @@ exports.default = function () {
     return new FrontEndLogger();
   }
   var runningScript = require.main.filename.split('/').pop();
-  var logDirPath = process.cwd() + '/log';
-  var logFilePath = logDirPath + '/' + runningScript + '.log';
   var streams = [];
-  try {
-    var logDirStats = _fs2.default.statSync(logDirPath);
-    if (logDirStats.isDirectory()) {
-      // create file if not exists
-      var fd = _fs2.default.openSync(logFilePath, 'a');
-      _fs2.default.closeSync(fd);
-
-      streams.push({
-        level: 'info',
-        path: logFilePath
-      });
-    }
-  } catch (e) {
-    console.log(e);
-  }
 
   streams.push({
     level: 'info',
